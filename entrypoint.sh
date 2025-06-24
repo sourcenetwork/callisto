3aa3010f199d7048ff5bbaf9fc079bd8b1cc2ff2
Bruno Gomes <gomes.bruno.ac@gmail.com>
2025-06-24 17:44:22 -0300

    fix: (entrypoint) create config if missing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 set -e
-if [ !  -e /home/callisto/.initialized ]; then
+if [ !  -e /etc/callisto/config.yaml ]; then
     callisto init --home /etc/callisto
+fi
+
+if [ !  -e /home/callisto/.initialized ]; then
     callisto parse genesis-file --home /etc/callisto --genesis-file-path /callisto/genesis.json
     touch /home/callisto/.initialized
 fi
